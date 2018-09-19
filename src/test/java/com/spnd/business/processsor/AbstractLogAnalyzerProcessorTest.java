e0e8f5db37e66b3cce5c3ec0197d76a043a6b8f5
sups149 <sups149@gmail.com>
2018-09-19 08:07:04 +0530

    Junit test updated.

--- src/test/java/com/spnd/business/processsor/AbstractLogAnalyzerProcessorTest.java
+++ src/test/java/com/spnd/business/processsor/AbstractLogAnalyzerProcessorTest.java
@@ -7,6 +7,7 @@ import com.spnd.data.entity.LogDetailsEntity;
 import com.spnd.data.repository.LogAnalyzerDao;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -17,11 +18,16 @@ import java.util.List;
 import java.util.Map;
 
 import com.spnd.helper.LogAnalyzerJunitHelper;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+@RunWith(PowerMockRunner.class)
 public class AbstractLogAnalyzerProcessorTest {
 
     @InjectMocks
@@ -36,13 +42,16 @@ public class AbstractLogAnalyzerProcessorTest {
     }
 
     @Test
+    @PrepareForTest(JsonUtil.class)
     public void testProcessGrouppedData() {
         Mockito.when(logAnalyzerDao.inserRecord(any(LogDetailsEntity.class))).thenReturn("42");
         Map<String, List<String>> grouppedData = LogAnalyzerJunitHelper.getDummyGrouppedData();
         spy(JsonUtil.class);
-        when(JsonUtil.calculateDuration(any(List.class))).thenReturn(10L);
-        System.out.println(JsonUtil.calculateDuration(new ArrayList<String>()));
-        //logAnalyzerProcessor.processGrouppedData(grouppedData);
+        List<String> jsonRecordList = LogAnalyzerJunitHelper.getDummyJsonRecordList();
+        when(JsonUtil.calculateDuration(jsonRecordList)).thenReturn(10L);
+        logAnalyzerProcessor.processGrouppedData(grouppedData);
+        verify(logAnalyzerDao, atLeastOnce()).inserRecord(any(LogDetailsEntity.class));
+
 
     }
 }
